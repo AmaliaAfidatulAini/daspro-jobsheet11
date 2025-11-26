bb5255c5d52c391608da6f5c5ab5a489da5c2ab2
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-11-26 21:38:36 +0700

    final modifikasi percobaan 3

--- Kafe03.java
+++ Kafe03.java
@@ -33,25 +33,48 @@ public class Kafe03 {
 
         Scanner sc = new Scanner(System.in);
 
-        Menu("Budi", true, "DISKON30");
+        String kodePromo = "DISKON30";
+        Menu("Budi", true, kodePromo);
 
-        System.out.print("Masukkan nomor menu: ");
-        int pilihanMenu = sc.nextInt();
-
-        System.out.print("Masukkan jumlah item: ");
-        int banyakItem = sc.nextInt();
-
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
-
-        System.out.println("Total harga untuk pesanan anda : Rp " + totalHarga);
-
-    }
-
-    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
         int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
 
-        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        int totalKeseluruhan = 0;
 
-        return hargaTotal; 
+        System.out.print("Berapa jenis menu yang ingin dipesan? ");
+        int jumlahJenis = sc.nextInt();
+
+        for (int i = 1; i <= jumlahJenis; i++) {
+            System.out.println("\nPesanan ke-" + i);
+
+            System.out.print("Masukkan nomor menu: ");
+            int pilihanMenu = sc.nextInt();
+
+            System.out.print("Masukkan jumlah item: ");
+            int jumlahItem = sc.nextInt();
+
+            int subtotal = hargaItems[pilihanMenu - 1] * jumlahItem;
+
+            totalKeseluruhan += subtotal;
+        }
+
+        totalKeseluruhan = hitungDiskon(totalKeseluruhan, kodePromo);
+
+        System.out.println("\n====================================");
+        System.out.println("Total akhir seluruh pesanan Anda: Rp " + totalKeseluruhan);
     }
+
+    public static int hitungDiskon(int totalHarga, String kodePromo) {
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid: Diskon 50%");
+            return totalHarga / 2;
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: Diskon 30%");
+            return totalHarga - (totalHarga * 30 / 100);
+        } else {
+            System.out.println("Kode promo invalid (Tidak ada diskon)");
+            return totalHarga;
+        }
+    }
+
 }
