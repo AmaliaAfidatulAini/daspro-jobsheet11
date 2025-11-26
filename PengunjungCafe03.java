3c7f663a24702e6d6e6d7b70a3201bb9f58ffdba
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-11-26 22:02:37 +0700

    percobaan 4

---
+++ PengunjungCafe03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PengunjungCafe03 {
+
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+
+    }
+    
+}
